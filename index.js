c1919971b623994ff2251dcffecdeb640f514fd7
Reina <r.clarkemitchell@gmail.com>
2019-10-26 19:22:08 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(arrOfNames){
+  for(let i = 0; i < arrOfNames.length; i++){
+    arrOfNames[i] = `Thank you, ${arrOfNames[i]}, for the wonderful surprise gift!`
+  }
+  return arrOfNames
+}
+
+
+function countDown(num){
+  for(let i = num; i >= 0; i--){
+    console.log(i)
+  }
+}
